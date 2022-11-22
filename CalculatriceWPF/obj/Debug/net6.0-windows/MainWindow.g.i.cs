66bdef3b32ebbd47dde965f8f4bd4e814bf29c31
Amadou Dioubate <dioubateamadou11@gmail.com>
2022-11-22 14:33:26 +0100

    Realisation projet dispatcher (timer)

--- CalculatriceWPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ CalculatriceWPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace CalculatriceWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CalculatriceWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CalculatriceWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
---
+++ Dispatcher/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dispatcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread? BgThead = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            //if(sender is not Button btn) return;
+            //ExecuteCounter(); Pa responsive
+
+            BgThead = new Thread(() => { ExecuteCounter(); });
+            BgThead.Start();
+        }
+
+        public void ExecuteCounter()
+        {
+            try
+            {
+                Dispatcher.Invoke(() => { btnStart.Content = "Counting..."; btnStart.IsEnabled=false; });
+
+
+                for (int i = 0; i <= 50; i++)
+                {
+                    //txtMessage.Text = i.ToString(); System.InvalidOperationException : 'Le thread appelant ne peut pas accéder à cet objet parce qu'un autre thread en est propriétaire.'
+                    Dispatcher.Invoke(() => { txtMessage.Text = i.ToString(); });
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
