5842dcb2fed0cb3fda485fe66e1869354b6df2d7
Amadou Dioubate <dioubateamadou11@gmail.com>
2022-11-23 09:35:01 +0100

    Création du projet NavigationMultiWindow

---
+++ NavigationMultiWindow/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NavigationMultiWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); // bug si on reconnait plus InitializeComponent();=> propriété du projet => decoche et coche activez WPF pour ce projet
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Menu 'New' Clicked !");
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Menu 'Open' Clicked !");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Menu 'Exit' Clicked !");
+        }
+    }
+}
