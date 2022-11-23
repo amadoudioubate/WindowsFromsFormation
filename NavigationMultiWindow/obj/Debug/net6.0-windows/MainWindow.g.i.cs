0ac4442e15c0baa2063ef4c105e35a1d2c3cd632
Amadou Dioubate <dioubateamadou11@gmail.com>
2022-11-23 12:13:07 +0100

    Création du projet UserControls

--- NavigationMultiWindow/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ NavigationMultiWindow/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace NavigationMultiWindow {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NavigationMultiWindow;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NavigationMultiWindow;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
