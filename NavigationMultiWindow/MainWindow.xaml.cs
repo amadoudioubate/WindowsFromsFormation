d575ba13624420df5492af4006349b3054f1204d
Amadou Dioubate <dioubateamadou11@gmail.com>
2022-11-23 11:57:51 +0100

    Fin projet Navigation

--- NavigationMultiWindow/MainWindow.xaml.cs
+++ NavigationMultiWindow/MainWindow.xaml.cs
@@ -15,21 +15,36 @@ using System.Windows.Shapes;
 
 namespace NavigationMultiWindow
 {
+
+    public static class CustomCommands
+    {
+        // Exit premier param qui sera affiché
+        // Exit 2eme Name me referencer
+        // 3ème Classe qui propriétaire typeof(CustomCommands)
+        // 4èeme param raccourci
+        public static readonly RoutedUICommand Exit = new(
+            "Exit",
+            "Exit",
+            typeof(CustomCommands),
+            new InputGestureCollection() { new KeyGesture(Key.X, ModifierKeys.Control) }
+            );
+
+        // Rajouter ici d'autres Command personnalisées
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent(); // bug si on reconnait plus InitializeComponent();=> propriété du projet => decoche et coche activez WPF pour ce projet
+            framePage.Content = new Page1(); // Au demarrage de mon appli on ouvre la Page1 par exemple (un choix)
         }
 
-        private void New_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Menu 'New' Clicked !");
-        }
-
+       
         private void Open_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Menu 'Open' Clicked !");
@@ -61,7 +76,32 @@ namespace NavigationMultiWindow
 
         private void Page2_Click(object sender, RoutedEventArgs e)
         {
+            framePage.Content = new Page2();
+        }
 
+        // Remplace
+        //private void New_Click(object sender, RoutedEventArgs e)
+        //{
+        //    MessageBox.Show("Menu 'New' Clicked !");
+        //}
+        private void NewCommandExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Menu 'New' Clicked  From Command with shortcut!");
+        }
+
+        private void NewCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ExitCommandExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current?.Shutdown();
+        }
+
+        private void ExitCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
